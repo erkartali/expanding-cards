8baf58e68b25a187000584b37b8825b03ef195a3
erkartali <erkartali@gmail.com>
2021-03-23 14:15:17 -0500

    added panels with background pictures, styles and javascript to expand and contract panels

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const panels = document.querySelectorAll(".card");
+console.log(panels);
 
+const removeActive = () => {
+  panels.forEach((element) => {
+    element.classList.remove("active");
+    element.children[0].style.display = "none";
+    console.log(element.children[0]);
+  });
+};
+
+panels.forEach((element) => {
+  element.addEventListener("click", () => {
+    removeActive();
+    element.classList.add("active");
+    element.children[0].style.display = "inline";
+  });
+});
